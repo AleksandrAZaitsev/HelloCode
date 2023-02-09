7973cbdd305fe40b8edaa0a9389b4521668ed7fa
Aleksandr <zaytsev.chel@gmail.com>
2023-02-09 18:23:07 +0500

    ADD Example006 IfElse

---
+++ Example006_IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же ");
+    Console.WriteLine(username);
+    }
+ else
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+ }   
+        
+    
